c14748509177c9e5efc160c72d9576fe70f4b0bf
tomygl1234 <tomasgimenezlascano123@gmail.com>
2024-08-19 01:30:57 +0200

    commit 2

--- database/migrations/2024_07_12_131403_create_posts_table.php
+++ database/migrations/2024_07_12_131403_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('content');
+            $table->text('description');
             $table->string('imagen');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
