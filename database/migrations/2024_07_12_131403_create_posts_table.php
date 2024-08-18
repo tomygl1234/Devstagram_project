6ad9b10bd9bca874c8c528492713703c406d76c3
tomygl1234 <tomasgimenezlascano123@gmail.com>
2024-08-19 01:37:14 +0200

    commit 3

--- database/migrations/2024_07_12_131403_create_posts_table.php
+++ database/migrations/2024_07_12_131403_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->string('imagen');
+            $table->string('image');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
